f3a77283c968a769e8e6bb08c674ce33f23b9a84
Eric Omanoff <ericomanoff@gmail.com>
2018-10-20 10:52:20 -0600

    updated name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     url: 'https://lumen.netlify.com',
-    title: 'Blog by John Doe',
+    title: 'Blog by Eric Omanoff',
     subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     copyright: '© All rights reserved.',
     disqusShortname: '',
